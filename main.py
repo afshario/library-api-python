0a8a382718d6b813a6164c8f932dcfa745de924a
MHAfshari <mhafshari1385@gmail.com>
2026-02-11 00:48:27 +0330

    sort book list by publication year

--- main.py
+++ main.py
@@ -17,16 +17,19 @@ def main():
             if(response.status_code != 200):
                   raise Exception(f"An error accures, http code : {response.status_code}")
             books = list()
-            for book in response.json()["docs"]:
+            for book in response.json().get("docs",""):
                   if book.get("first_publish_year",0) > 2000 :
                         books.append(book)
-
+            sorted_books = sorted(
+                  books, 
+                  key= lambda x: x.get("first_publish_year",0)
+            )
             with open('result.csv', 'w', newline='', encoding='utf-8') as file:
                   fieldnames = ['title', 'first_publish_year', 'author_name',"language"]
                   writer = csv.DictWriter(file, fieldnames=fieldnames)
-        
                   writer.writeheader()
-                  for book in books:
+
+                  for book in sorted_books:
                         row = {
                         'title': book.get('title', ''),
                         'first_publish_year': book.get('first_publish_year', ''),
